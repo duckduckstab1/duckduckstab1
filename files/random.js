6054a7d1592eaf153241bd1689aadb613f903c20
hurricane495 <84234115+duckduckstab1@users.noreply.github.com>
2022-09-18 18:40:55 +0100

    Create random.js

---
+++ files/random.js
@@ -0,0 +1,151 @@
+var strings = [
+
+	'hi',	'what',
+
+	'i dont see the point of this tbh',
+
+	'i dislike tbh creature',
+
+	'NO ONE CARES ABOUT TBH CREATURE IT IS ARGABAE!!!',
+
+	'ratitio',
+
+	'garabage',
+
+	'HEY FAHGOLAEH, I TIHNKHK UHERE EREHETAHRERDD!!!!!!!!!!!!!!!!!!!!!!!!!',
+
+	'?',
+
+	'? You should start thinking about it.',
+
+	'poo poo doodie too / weewee urine peepee poo / poo pee peepee poo',
+
+	'"AMONG US" "AMOGUS" "SUS" "WHEN THE IMPOSTER IS SUS!" "SUSSY BAKA" bitch shut the fuck up are you\'re a schizo or something',
+
+	'Crack.',
+
+	'oeptos desktop friend crack downloader keygen',
+
+	'HELP I ATE MY HAND!!!',
+
+	'Off to visit your mother!',
+
+	'fuck y',
+
+	'The EL.',
+
+	'|||||||||||||||||||||||||||| PIPE ERRORE! |||||||||><<>><,.<>><> WTF SHIT FUCK BRAIN FUCK!!!!!!!!!!!! >ME<";fg[q\'joegnLAE-[pjake',
+
+	'pull a javascript out of yo a',
+
+	'the 16 bit integer limit will over flow and corrupt the java script',
+
+	'Z',
+
+	'collabvm sucks ass',
+
+	'THE / AWOGLET IMPORTANT / SEOHLRY @BANANA you are stupid / WHY IMPORTANT / HOW',
+
+	'awoglet language minifier',
+
+	'have you evr heard of obfuscator.io',
+
+	'REHTRAHRHDHD FHGAHGHTBTHGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+
+	'dunno',
+
+	'I MADE',
+
+	'MMmmmmm. Snawhich.',
+
+	'are you ok',
+
+	'are you drunk',
+
+	':shut up folding chair go fold or something',
+
+	'GAY!!!!!! HAHAHAHAHAHAHAHA IM SO FUNNY',
+
+	'there was doo doo feces',
+
+	'You\'re a monster. YOU RUINED MY LIFE!',
+
+	'Shut the fuck Caarcakrhead',
+
+	'crakkckkaheajdjheehaddddd',
+
+	':crack:',
+
+	'i poopyed fart',
+
+	':AWOSI',
+
+	'Wehhehe THe FUCK',
+
+	'Dun dun dun dudu dududn dudndMPD:SGlSDGSLD"g',
+
+	'Rust because the code rusts over time hahadhahhadhkfadjm;a/ldoghalen;jamw;egoaegmalwgweeg',
+
+	'AWOGLET INSANITY!',
+
+	'ok damn :crabl',
+
+	':Feature my balls in 3d',
+
+	'the big shitgle',
+
+	'A{R{EAR}A{ER{}',
+
+	'STOP HAVIEING A STROKE!!!!!!!!!! :nerd:',
+
+	'Who thinks they\'re a better game than I',
+
+	'i will break my spine help',
+
+	'yooo we have images now',
+
+	'<img src="https://cdn.discordapp.com/attachments/543398541381468201/990244238925103164/caption.gif"/>',
+
+	'<img src="https://static.wikia.nocookie.net/d9d4536f-58cb-4c07-9c8a-ffd66f86305e"/>',
+
+	'<img src="https://cdn.discordapp.com/attachments/543398541381468201/985929351977648128/caption.gif"/>',
+
+	'NIIIIIIIIIGGGGEEEEEEEEEEEEERRRRRRR. Oh \'scuse me sir! Anyway I made this web OS as a fork of 98.js, a web OS for people wanting to try Windows 98 without having to utilize a Virtual Machine. I used the latest version and since the official builds use modern JavaScript, I can run my web OS as low as Safari 10 with no trouble. I can also port my web OS to almost any OS with just Electron! Isn\'t that cool? But wait, there\'s more! It adds a few apps, and changes the design. Very cool, isn\'t it? I also plan to make it 100 times better than Windows 93 and Windows 96 combined, by adding a few more apps and changing the design. That\'s really cool, don\'t you think?',
+
+	'<img src="https://cdn.discordapp.com/attachments/543398541381468201/981771853985103992/meme.png"/>',
+
+	'<img src="https://cdn.discordapp.com/attachments/543398541381468201/981091548559204362/caption.gif"/>',
+
+	'<img src="https://cdn.discordapp.com/attachments/543398541381468201/994518777184124988/caption.gif"/>',
+
+	'<img src="https://cdn.discordapp.com/attachments/543398541381468201/973533961206784070/caption.png"/>',
+
+	'Hey did you just say WAUGMP WAUGHMP .',
+
+	'what you wanna play you wanna bully me? never come back again',
+
+	'collabvm more like RETARD RETARD FORKIE FORKIE FORKIEFAG GUESTFAG HAHAHAHAHAHAAHAHAHAHAHAHA U FORKIE THE VM I WILL BAN YOU FISH FISH FISH BOOHBAH HORSE PORN HORSE PORN C++ CUM SEX CAB JACKS OFF TO HIASM AND HAS NO LIFE NIGGER NIGGER NIGGER NIGGER ANDERJ IS NIGGER FAG',
+
+	'collabvm more like drunk asss',
+
+	'"Was a criminal, always a criminal." -cancel culture (and of course collabvm)',
+
+	'why am i copying "i will break my spine help"',
+
+	'why is "Y o u  n e e d  m e ." the last line on this list',
+
+	'ok fine hiasm sucks but you are telling me that like i will care',
+
+	'AWOSINO GHLET!',
+
+	'please leave a message after the beep. BEEEEEEEEEEEEEEEEEEEEEEEEEP! PAH!',
+
+	'Y o u  n e e d  m e .'
+
+];
+
+var el = document.createElement('p');
+
+el.innerHTML = strings[Math.floor(Math.random() * strings.length)];
+
+document.body.appendChild(el);
